c74e4bd402de7568ea75b06b1412f078c676f8cd
kshy <habxp@163.com>
2016-05-12 09:47:20 +0800

    first init

---
+++ README.md
@@ -0,0 +1,9 @@
+### 处理html页面或者isHtmlLike的模板js中的相对路径或者动态生成的src链接
+
+> fis3插件
+
+
+```javascript
+    //配置
+    postpackager : fis.plugin('tagpath',{ domain:'/AsYouWish'})
+```
---
+++ index.js
@@ -0,0 +1,75 @@
+'use strict';
+
+module.exports = function(ret, conf, settings, opt) {
+
+    //
+    // var startReg = /<!--\s*build:(\w+)(?:\(([^\)]+?)\))?\s+(\/?([^\s]+?))\s*-->/gim;
+    // var endReg = /<!--\s*endbuild\s*-->/gim;
+
+    //替换a标签和css或者js中的资源
+    var tagReg = [/<a[^>]+href=([^>]+)>/gi, /<link[^>]+href=([^>]+)>/gi, /<script[^>]+src=([^>]+)>/gi, /<img[^>]+src=([^>]+)>/gi];
+
+    var aTagReg = /<a[^>]+href=([^>]+)>/gi;
+    //重组打包对象
+    var map = {};
+    fis.util.map(ret.src, function(subpath, file) {
+
+        if (!file.isHtmlLike && file.ext != ".tmpl") {
+
+            map[subpath.replace("/src/", "")] = file.getUrl(opt.hash, opt.domain);
+        }
+        // console.log('\n'+subpath+' === '+file.getUrl())
+    });
+
+    //concat
+    fis.util.map(ret.src, function(subpath, file) {
+
+        //html类文件，才需要做替换
+        if (file.isHtmlLike) {
+            var content = file.getContent();
+
+            // for(var i=0;i<tagReg.length;i++){
+
+            //     content = content.replace(tagReg[i],function(tag,url){
+
+            //         if(url.indexOf("http://") > -1)return tag;
+
+            //         // if(url.indexOf('test/a')>-1) console.log(url);
+
+            //         url = url.replace(/"|'/g,"");
+
+            //         url = url.split(" ")[0];
+            //         //url = url.replace("../","");
+            //         var pkg = map[url.replace("../","")];
+            //         // console.log(url,pkg)
+
+            //         if(pkg){
+
+            //             return tag.replace(url,'xxxxx');
+            //         }
+            //         return tag;
+            //     });
+
+            // }
+            //不行啊 这里还要去除javascript:; http://
+            var speTag = /http|(javascript:[;|(void(0)])/;
+            content = content.replace(aTagReg, function(tag, url) {
+                console.log('\n' + url)
+                if (speTag.test(url)) {
+                    console.log(url)
+                    return tag;
+                }
+
+                if (settings && settings.domain) {
+                    url = url.replace(/"|'/g, "");
+
+
+                    return tag.replace(url, settings.domain + url)
+                }
+
+            })
+
+            file.setContent(content);
+        }
+    });
+};
